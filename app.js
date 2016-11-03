7906642fb783860ce5e279c528865bb71d20fb09
Carmine Biello <nucciodream@gmail.com>
2016-11-03 22:23:02 +0100

    Name value calculator exercise complete!

--- app.js
+++ app.js
@@ -4,7 +4,23 @@
   angular.module('NameCalculator', [])
 
   .controller('NameCalculatorController', function ($scope) {
-    $scope.totalValue = 100;
+    $scope.name="";
+    $scope.totalValue = 0;
+
+    $scope.displayNumeric = function(){
+      var totalNameValue = CalculatNumericForString($scope.name);
+      $scope.totalValue = totalNameValue;
+    };
+
+    function CalculatNumericForString(string){
+      var totalStringValue = 0;
+      for(var i=0; i<string.length; i++){
+          totalStringValue += string.charCodeAt(i);
+      }
+
+      return totalStringValue;
+    }
+
 
   });
 
